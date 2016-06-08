91757f7b72fc308366e36ba7692586e392a2a9a6
Chris Nelson <cnelson@cnelson.org>
2016-06-07 17:55:42 -0700

    additional simplifications

--- jobs/clamav/templates/bin/clamav_ctl.sh
+++ jobs/clamav/templates/bin/clamav_ctl.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 export PKG_LOC=/var/vcap/packages/clamav
 
-cp /var/vcap/jobs/clamav/conf/freshclam.conf $PKG_LOC/etc/freshclam.conf 
-cp /var/vcap/jobs/clamav/conf/clamd.conf $PKG_LOC/etc/clamd.conf 
 LOG_DIR=/var/vcap/sys/log/clamav
 RUN_DIR=/var/vcap/sys/run/clamav
 
@@ -18,11 +16,11 @@ fi
 
 case "$1" in
 	'start_clamd')
-	  $PKG_LOC/sbin/clamd -c $PKG_LOC/etc/clamd.conf
+	  $PKG_LOC/sbin/clamd -c /var/vcap/jobs/clamav/conf/clamd.conf
 	  sleep 1
 	;;
 	'start_freshclam')
-	  $PKG_LOC/bin/freshclam -d --config-file $PKG_LOC/etc/freshclam.conf
+	  $PKG_LOC/bin/freshclam -d --config-file /var/vcap/jobs/clamav/conf/freshclam.conf
 	;;
 	'stop_clamd')
 	  kill `pidof clamd`
