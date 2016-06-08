d98cb7720aff1a937f2bc95d3ea785b16501c14a
Chris Nelson <cnelson@cnelson.org>
2016-06-07 17:36:36 -0700

    updating package to 0.99 + use freshclam to sync initial database at compile time

--- jobs/clamav/templates/bin/clamav_ctl.sh
+++ jobs/clamav/templates/bin/clamav_ctl.sh
@@ -3,32 +3,17 @@ export PKG_LOC=/var/vcap/packages/clamav
 
 cp /var/vcap/jobs/clamav/conf/freshclam.conf $PKG_LOC/etc/freshclam.conf 
 cp /var/vcap/jobs/clamav/conf/clamd.conf $PKG_LOC/etc/clamd.conf 
-#
-# /etc/init.d/daemon-clamd is not needed anymore and may be present when upgrading an earlier deployment
-if [ -f /etc/init.d/daemon-clamd ]
-then
-  rm -f /etc/init.d/daemon-clamd
-fi
-
 LOG_DIR=/var/vcap/sys/log/clamav
 RUN_DIR=/var/vcap/sys/run/clamav
+
 if [ ! -d "${LOG_DIR}" ]; then
 	mkdir -p ${LOG_DIR}
 	touch ${LOGDIR}/freshclam.log
 	touch ${LOG_DIR}/clamav.log
 fi
+
 if [ ! -d "${RUN_DIR}" ]; then
 	mkdir -p ${RUN_DIR}
-fi
-if [ ! -d "/var/lib/clamav" ]; then
-	mkdir -p /var/lib/clamav
-fi
-
-# Copy virus definitions if it is not there already
-if [ ! -f /var/lib/clamav/main.cvd ]; then
-	cp $PKG_LOC/virus_defs/main.cvd /var/lib/clamav/
-	cp $PKG_LOC/virus_defs/daily.cvd /var/lib/clamav/
-	cp $PKG_LOC/virus_defs/bytecode.cvd /var/lib/clamav/
 fi
 
 case "$1" in
